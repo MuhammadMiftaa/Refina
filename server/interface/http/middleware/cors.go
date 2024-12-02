0f385ed958adbc806b4e4000357dc527e8c3f30e
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2024-12-02 09:59:00 +0700

    configure allow origin in cors

--- server/interface/http/middleware/cors.go
+++ server/interface/http/middleware/cors.go
@@ -9,7 +9,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     *,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
