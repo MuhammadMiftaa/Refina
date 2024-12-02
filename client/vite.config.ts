47cb685ae5b67c2cde86e0776e6a3a394041e41c
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2024-12-02 10:18:18 +0700

    setup vercel.json to setting server-side routing🐳

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,12 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    open: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
