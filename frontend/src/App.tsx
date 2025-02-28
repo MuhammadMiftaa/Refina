02dda87f1e0f65ec8134170a811cab58688f386b
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2025-02-28 14:46:15 +0700

    setup pwa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
-import { useState } from "react";
-import Cookies from "js-cookie";
+// import { useState } from "react";
+// import Cookies from "js-cookie";
 import MainLayout from "./components/layouts/MainLayout";
 import Home from "./components/pages/Home";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(() => {
-    return Cookies.get("token") ? true : false;
-  });
+  // const [isAuthenticated, setIsAuthenticated] = useState(() => {
+  //   return Cookies.get("token") ? true : false;
+  // });
 
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-  };
+  // const handleLogin = () => {
+  //   setIsAuthenticated(true);
+  // };
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
---
+++ refina/vite.config.ts
@@ -0,0 +1,59 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      registerType: "autoUpdate",
+      injectManifest: {
+        swDest: "dist/sw.js",
+      },
+      manifest: {
+        name: "Refina",
+        short_name: "Refina",
+        description: "Rekapan Finansialmu",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "#E6E6FA",
+        background_color: "#E6E6FA",
+        start_url: "/",
+        display: "standalone",
+        orientation: "portrait",
+        // gcm_sender_id: "131099091498"
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+});
