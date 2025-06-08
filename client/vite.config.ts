66479006c5458c2501c4a580365e3812e8632a79
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2025-06-08 20:22:47 +0700

    modify vite config at client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+if (!process.env.VITE_API_URL) {
+  throw new Error('VITE_API_URL is not defined');
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   // server: {
