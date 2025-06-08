43ed116be597df2c31217a248b8c1b545a51e85a
Muhammad Miftakul Salam <muhammadmiftakul.22101@mhs.unesa.ac.id>
2025-06-08 20:21:31 +0700

    modify getBackendURL at client

--- client/src/lib/readenv.ts
+++ client/src/lib/readenv.ts
@@ -1,2 +1,8 @@
 export const getMode = () => import.meta.env.VITE_MODE;
-export const getBackendURL = (): string => import.meta.env.VITE_API_URL;
+export const getBackendURL = (): string => {
+  const url = import.meta.env.VITE_API_URL;
+  if (!url) {
+    throw new Error("VITE_API_URL is not defined");
+  }
+  return url;
+};
