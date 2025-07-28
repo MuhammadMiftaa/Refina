c713052bd6886dba598f074dd987f308ecdd59f6
Muhammad Mifta <madmifta77@gmail.com>
2025-07-28 15:55:48 +0700

    change logo

--- client/src/components/ui/team-switcher.tsx
+++ client/src/components/ui/team-switcher.tsx
@@ -26,7 +26,7 @@ export function TeamSwitcher() {
           size="lg"
           className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
         >
-          <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center overflow-hidden rounded-lg">
+          <div className="text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center overflow-hidden rounded-lg">
             <img src="/logo.png" alt="Logo Refina" />
           </div>
           <div className="grid flex-1 text-left text-sm leading-tight">
